c47394e96cb4615f89185391a998710ab552b5d3
Rémi Emonet <remi.emonet@reverse--com.heeere>
2019-04-16 08:12:30 +0200

    Minimal example with a matplotlib file-based plot

---
+++ example-4.py
@@ -0,0 +1,30 @@
+
+import asyncio
+import numpy as np
+import matplotlib.pyplot as plt
+
+from vuejspython import model, start
+
+
+@model
+class Comp:
+    # define properties to forward to vue (or not)
+    i = 42
+    fname = ""
+
+    def __init__(self):
+        self.i = 1
+
+    def watch_i(self, i):
+        fname = ",,fname-"+str(self.i)+".svg"
+        x = np.linspace(0, 100, 1000)
+        y = x-i
+        y[y<0] = 0
+        plt.figure()
+        plt.grid()
+        plt.plot(x, y)
+        plt.savefig(fname)
+        self.fname = fname
+        
+
+start(Comp())
