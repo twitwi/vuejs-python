77006c94c6e9658240d30afa1bc36ec415beba17
Rémi Emonet <remi.emonet@reverse--com.heeere>
2019-04-17 18:34:52 +0200

    Use more computed values (need delayed recompute to have it fully ok)

--- example-6.py
+++ example-6.py
@@ -15,8 +15,10 @@ class Comp:
     yp = np.zeros((1,))
     xm = np.zeros((1,))
     ym = np.zeros((1,))
+    _v_nobroadcast = ['d1m', 'd1p']
 
     def __init__(self):
+        self.draw_set2pluses()
         self.draw_dataset()
 
     def computed_xmin(self): return self.x.min()
@@ -25,21 +27,37 @@ class Comp:
     def computed_ymin(self): return self.y.min()
     def computed_ymax(self): return self.y.max()
     def computed_dy(self): return (self.y.max() - self.y.min()) / (np.sum(self.y.shape)-1)
+    def computed_d1m(self): return np.min( ((self.x-self.xm)**2 + (self.y-self.ym)**2)**0.5, axis=2)
+    def computed_d1p(self): return np.min( ((self.x-self.xp)**2 + (self.y-self.yp)**2)**0.5, axis=2)
+    def computed_im(self): return np.transpose(self.d1m / (self.d1p+.0001))
 
     def draw_dataset(self):
-        self.y = np.linspace(-0.87, 0.87, 103)[None, :, None]
         self.x = np.linspace(-1.3, 1.3, 91)[:, None, None]
-        self.xp = np.array([0, 0.75])[None,None,:]
-        self.yp = np.array([0, 0])[None,None,:]
+        self.y = np.linspace(-0.87, 0.87, 103)[None, :, None]
         rangle = np.random.uniform(0, np.pi*4/3, (1, 1, 300))
         rradius = np.random.uniform(1, 1.7, rangle.shape)
         self.xm = rradius * np.cos(rangle)
         self.ym = rradius*4/6 * np.sin(rangle)
-        d1m = np.min( ((self.x-self.xm)**2 + (self.y-self.ym)**2)**0.5, axis=2)
-        d1p = np.min( ((self.x-self.xp)**2 + (self.y-self.yp)**2)**0.5, axis=2)
-        s = d1m/(d1p+.0001)
-        self.im = np.transpose(s)
 
+    def draw_bonedataset(self, t='bone', N=300):
+        x = np.linspace(-1.3, 1.3, 1000)[:,None]
+        y = np.linspace(-0.6, 0.6, 600)[None,:]
+        if t == 'bone':
+            candidate = (y/0.5)**2 + (x)**2 > 1
+            candidate |= ((y-0.5)/0.4)**2 + (x/0.5)**2 < 1
+            candidate |= ((y+0.5)/0.4)**2 + (x/0.5)**2 < 1
+        else:
+            candidate = (y/0.5)**2 + (x)**2 > 1
+        candx,candy = np.where(candidate)
+        candind = np.random.choice(range(candx.shape[0]), N, replace=False)
+        self.xm = x[None,None,candx[candind],0]
+        self.ym = y[None,None,0,candy[candind]]
 
+    def draw_set2pluses(self):
+        self.xp = np.array([0, 0.75])[None,None,:]
+        self.yp = np.array([0, 0])[None,None,:]
 
+    def draw_somepluses(self, N=20):
+        self.xp = np.random.uniform(self.xmin, self.xmax, (1,1,N))
+        self.yp = np.random.uniform(self.ymin, self.ymax, (1,1,N))
 start(Comp())
